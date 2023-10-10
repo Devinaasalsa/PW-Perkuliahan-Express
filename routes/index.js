439a47e9bd4fc064d51e324b6ff931df6eb5bbb3
Devinaasalsa <devinasdiva@gmail.com>
2023-10-10 11:22:57 +0700

    update mhs controller

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ const mahasiswaController = new MahasiswaController();
 
 /* GET home page. */
 router.get('/getMahasiswa', mahasiswaController.getAllMahasiswa);
+router.get('/getMahasiswaById/:id', mahasiswaController.getMahasiswaById)
 router.post('/createMahasiswa', mahasiswaController.createMahasiswa);
+router.patch('/updateMahasiswa/:id', mahasiswaController.updateMahasiswa);
+router.delete('/deleteMahasiswa/:id', mahasiswaController.deleteMahasiswa);
 
 module.exports = router;
