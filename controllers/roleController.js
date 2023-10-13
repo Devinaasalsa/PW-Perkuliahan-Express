b7fc15ab7a7f4f145578a14883ade94097fe2057
Devinaasalsa <devinasdiva@gmail.com>
2023-10-13 14:09:13 +0700

    refactor(matkulController):add getMatkulById, refactor(roleController):add some routes at role

--- controllers/roleController.js
+++ controllers/roleController.js
@@ -4,34 +4,92 @@ const prisma = new PrismaClient();
 
 
 class RoleController {
-    async createRole(req, res) {
-        const { roleName } = req.body;
-        try {
-          const existingRole = await prisma.role.findUnique({
-            where: {
-              roleName: roleName,
-            },
-          });
-    
-          if (existingRole) {
-            return res.status(500).json({
-              error: "Tidak bisa menambahkan role, role telah terdaftar",
-            });
-          }
-          const newRole = { roleName };
-          const roles = await prisma.role.create({
-            data: {
-              ...newRole
-            },
-          });
-          res.json(roles);
-        } catch (error) {
-          console.error("Terjadi kesalahan saat menambahkan Role", error);
-          res
-            .status(500)
-            .json({ error: "Terjadi kesalahan saat menambahkan Role" });
-        }
+  async getAllRole(req, res) {
+    try {
+      const roles = await prisma.role.findMany();
+      res.status(200).json(roles);
+    } catch (error) {
+      console.error("Terjadi kesalahan saat menampilkan data Role", error);
+      res
+        .status(500)
+        .json({ error: "Terjadi kesalahan saat menampilkan data Role" });
     }
+  }
+
+  async getRoleById(req, res) {
+    const { id } = req.params;
+    try {
+      const roles = await prisma.role.findFirst({
+        where: {
+          id: parseInt(id),
+        },
+      });
+      if (!roles) {
+        return res.json(400).json({ error: "Role tidak ditemukan" });
+      }
+      res.json(roles);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Terjadi kesalahan saat menampilkan data role" })
+    }
+  }
+
+  async createRole(req, res) {
+    const { roleName } = req.body;
+    try {
+      const existingRole = await prisma.role.findUnique({
+        where: {
+          roleName: roleName,
+        },
+      });
+
+      if (existingRole) {
+        return res.status(500).json({
+          error: "Tidak bisa menambahkan role, role telah terdaftar",
+        });
+      }
+      const newRole = { roleName };
+      const roles = await prisma.role.create({
+        data: {
+          ...newRole
+        },
+      });
+      res.json(roles);
+    } catch (error) {
+      console.error("Terjadi kesalahan saat menambahkan Role", error);
+      res
+        .status(500)
+        .json({ error: "Terjadi kesalahan saat menambahkan Role" });
+    }
+  }
+  async updateRole(req, res){
+    const { roleName } = req.body;
+    const { id } = req.params;
+    try {
+        const roles = await prisma.role.update({
+            where: {id:parseInt(id)},
+            data: {roleName}
+        })
+        res.status(200).json(roles);
+    }catch(error) {
+        console.log(error)
+        res.status(500).json({ error: "Terjadi kesalahan saat update data role" });
+    }
+}
+
+async deleteRole (req, res) {
+    const {id} = req.params;
+
+    try {
+        const roles = await prisma.role.delete({
+            where: {id:parseInt(id)},
+        })
+        res.status(200).json(roles);
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: "Terjadi kesalahan saat menghapus data role" });
+      }
+}
 }
 
 module.exports = RoleController;
--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ const acaraBeritaController = new AcaraBeritaController();
 const absensiController = new AbsensiController();
 const roleController = new RoleController();
 
-/* GET home page. */
+// routes mahasiswa
 router.get('/getMahasiswa', mahasiswaController.getAllMahasiswa);
 router.get('/getMahasiswaById/:id', mahasiswaController.getMahasiswaById)
 router.post('/createMahasiswa', mahasiswaController.createMahasiswa);
@@ -28,18 +28,23 @@ router.post('/createDosen', dosenController.createDosen);
 router.patch('/updateDosen/:id', dosenController.updateDosen);
 router.delete('/deleteDosen/:id', dosenController.deleteDosen);
 
-
+//routes matkul
 router.get('/getMatkul', matkulController.getAllMatkul);
-// router.get('/getMahasiswaById/:id', mahasiswaController.getMahasiswaById)
+router.get('/getMatkulById/:id', matkulController.getMatkulById);
 router.post('/createMatkul', matkulController.createMatkul);
 router.patch('/updateMatkul/:id', matkulController.updateMatkul);
 router.delete('/deleteMatkul/:id', matkulController.deleteMatkul);
 
-
+//routes absensi
 router.get('/getAbsen', absensiController.getAbsensi);
 router.post('/inputAbsen', absensiController.inputAbsensi);
 
+//routes role
+router.get('/getRole', roleController.getAllRole);
+router.get('/getRoleById/:id', roleController.getRoleById);
 router.post('/createRole', roleController.createRole);
+router.patch('/updateRole/:id', roleController.updateRole)
+router.delete('/deleteRole/:id', roleController.deleteRole)
 
 //rute of acara berita page
 router.get('/getAcaraBerita', acaraBeritaController.getAllAcaraBerita)
