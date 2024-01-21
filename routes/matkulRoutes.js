421593ca899a1baa50530f1645fe12245cacb698
Devinaasalsa <devinasdiva@gmail.com>
2024-01-21 09:43:28 +0700

    fix:middleware matkul

--- routes/matkulRoutes.js
+++ routes/matkulRoutes.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const DosenController = require('../controllers/dosenController');
-const MiddlewareDosen = require('../middleware/middlewareDosen');
+const MiddlewareAdmin = require('../middleware/middlewareAdmin');
 const MiddlewareMahasiswa = require('../middleware/middlewareMahasiswa');
 const MatkulController = require('../controllers/matkulController');
 
 const router = express.Router();
 const dosenController = new DosenController();
 const matkulController = new MatkulController();
-const middlewareDosen = new MiddlewareDosen();
+const middlewareAdmin = new MiddlewareAdmin();
 const middlewareMahasiswa = new MiddlewareMahasiswa();
 
-// Define Dosen routes
-router.get('/getMatkul', middlewareDosen.isDosen, middlewareMahasiswa.isMahasiswa, matkulController.getAllMatkul);
+// Define Matkul routes
+router.get('/getMatkul', matkulController.getAllMatkul);
 router.get('/getMatkulById/:id', matkulController.getMatkulById);
 router.post('/createMatkul', matkulController.createMatkul);
-router.patch('/updateMatkul/:id',  middlewareDosen.isDosen, matkulController.updateMatkul);
-router.delete('/deleteMatkul/:id', middlewareDosen.isDosen, matkulController.deleteMatkul);
+router.patch('/updateMatkul/:id',  middlewareAdmin.isAdmin, matkulController.updateMatkul);
+router.delete('/deleteMatkul/:id', middlewareAdmin.isAdmin, matkulController.deleteMatkul);
 router.patch('/updateMatkul/:id', matkulController.updateMatkul);
 router.delete('/deleteMatkul/:id', matkulController.deleteMatkul);
 router.get('/searchMatkul?', matkulController.searchMatkul);
